38fd4e28157f9f1aafddeeaba2d55eec99893194
ggoachet <goachetgerard@gmail.com>
2019-03-12 16:00:45 +0100

    modified model post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+	belongs_to: user
+	belongs_to: theme
+	has_many: comments
+	has_many: likes
+	has_many :users, through: :likes
+	has_many :comments,as: :commentable
+	has_many :tagitems,as: :tagable
 end
