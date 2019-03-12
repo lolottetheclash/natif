9fcd240b997c27368d42de4e49fea12a8f3443a4
selmenm <selmen.mhamdi@live.fr>
2019-03-12 16:44:04 +0100

    scaffold de review

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :reviews
   resources :carts
   resources :items
   devise_for :users
---
+++ spec/views/reviews/new.html.erb_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "reviews/new", type: :view do
+  before(:each) do
+    assign(:review, Review.new(
+      :title => "MyString",
+      :content => "MyText",
+      :item => nil,
+      :user => nil,
+      :rating => 1
+    ))
+  end
+
+  it "renders new review form" do
+    render
+
+    assert_select "form[action=?][method=?]", reviews_path, "post" do
+
+      assert_select "input[name=?]", "review[title]"
+
+      assert_select "textarea[name=?]", "review[content]"
+
+      assert_select "input[name=?]", "review[item_id]"
+
+      assert_select "input[name=?]", "review[user_id]"
+
+      assert_select "input[name=?]", "review[rating]"
+    end
+  end
+end
