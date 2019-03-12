1ec12bf453ffbc44402fe4481f5def5aae3df1ff
lauren <laurenzolivier@gmail.com>
2019-03-12 15:26:44 +0100

    Model Order created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_134152) do
+ActiveRecord::Schema.define(version: 2019_03_12_142025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,18 @@ ActiveRecord::Schema.define(version: 2019_03_12_134152) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "stripe_id"
+    t.string "address"
+    t.string "zipcode"
+    t.bigint "user_id"
+    t.bigint "delivery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["delivery_id"], name: "index_orders_on_delivery_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "roles", force: :cascade do |t|
@@ -73,4 +85,6 @@ ActiveRecord::Schema.define(version: 2019_03_12_134152) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "deliveries"
+  add_foreign_key "orders", "users"
 end
