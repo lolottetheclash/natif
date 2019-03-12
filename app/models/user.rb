dcaeafcd042207aa287d564e6e0f776fb19b1fb6
selmenm <selmen.mhamdi@live.fr>
2019-03-12 15:27:23 +0100

    Setting up database relations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, :confirmable
+
+  has_many :role_associations
+  has_many :roles, through: :role_associations
+
+  has_many :orders
+  has_many :carts
+  has_many :reviews
+  has_many :wishlists
+  has_many :likes
+  has_many :comments
+
 end
