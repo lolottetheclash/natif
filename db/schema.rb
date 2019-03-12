6c0057fb68ac97913060335d4fc5fc9a91ec6202
guillaume-CHR <guillaume.christe@gmail.com>
2019-03-12 18:16:03 +0100

    SEED

--- db/schema.rb
+++ db/schema.rb
@@ -99,10 +99,9 @@ ActiveRecord::Schema.define(version: 2019_03_12_144247) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "orders", "deliveries"
-  add_foreign_key "orders", "users"
 end
