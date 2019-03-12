f8621f6339bbe1513deeb3cd0203bb821949e93f
ggoachet <goachetgerard@gmail.com>
2019-03-12 16:35:02 +0100

    Files model and migrate for table Likes modified

--- db/migrate/20190312152815_create_likes.rb
+++ db/migrate/20190312152815_create_likes.rb
@@ -1,7 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
-
+      t.belongs_to :user, index: true
+      t.belongs_to :post, index: true
       t.timestamps
     end
   end
