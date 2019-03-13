3e017121080581bff93aa55e1dc8597d5ff6d1b9
guillaume-CHR <guillaume.christe@gmail.com>
2019-03-13 10:03:57 +0100

    Update app/model/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,18 @@ class User < ApplicationRecord
          
   has_many :role_associations
   has_many :roles, through: :role_associations
+
   has_many :orders
-  has_many :carts
+
+  has_many :variants
+  has_many :carts, through: :variants
+  has_many :wishlists, through: :variants
+
   has_many :reviews
-  has_many :wishlists
   has_many :likes
   has_many :comments
-  has_many :posts
+  
+  has_many :posted_items, foreign_key: 'author_id', class_name: "Item"
+  has_many :posted_articles, foreign_key: 'author_id', class_name: "Post"
+
 end
