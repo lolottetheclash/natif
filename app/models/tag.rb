241dd7d15a623276ab8114a8c7d6d190bd792c84
guillaume-CHR <guillaume.christe@gmail.com>
2019-03-13 10:10:24 +0100

    Update app/model/tag.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,6 @@
 class Tag < ApplicationRecord
-	has_many :targets, through: :tag_item
+	has_many :tag_items
+	has_many :variants, through: :tag_items
+	has_many :posts, through: :tag_items
+	has_many :items, through: :tag_items
 end
