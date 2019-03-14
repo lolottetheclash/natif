9342ce6ad039a5daf5fbaea094d021dffb354b61
selmenm <selmen.mhamdi@live.fr>
2019-03-14 23:31:40 +0100

    Creation of Header, Footer and Modal menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
+
   root 'variants#index' 
-  resources :wishlists
+  
   resources :variants
-  resources :posts
+  resources :wishlists
   resources :reviews
-  resources :carts
   resources :items
-  devise_for :users
+
+  resources :posts
+  get "/index_list.html.erb", to: "posts#index_list"
+
+
+ 
+  resources :carts
   get '/add_to_carts/:id' => 'carts#additem', as: 'addtocart'
   get '/remove_from_carts/:id' => 'carts#removeitem', as: 'removefromcart'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/index_list.html.erb", to: "posts#index_list"
+
+  devise_for :users
+
 end
