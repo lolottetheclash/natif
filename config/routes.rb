6a7284a4205d42fc087e2cd80855112d99a4d975
lauren <laurenzolivier@gmail.com>
2019-03-14 12:29:07 +0100

    new registration.show

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   resources :carts
   resources :items
   devise_for :users
+  devise_scope :user do 
+    get "/my_profile" => "registrations#show"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
