094f88da27403293c6ba0dea84d054b9c66e184d
selmenm <selmen.mhamdi@live.fr>
2019-03-18 15:54:59 +0100

    Recursive deletion done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -54,6 +54,10 @@ class ItemsController < ApplicationController
   # DELETE /items/1
   # DELETE /items/1.json
   def destroy
+    varantsofitem = Variant.where(item_id: @item.id)
+    varantsofitem.each do |element|
+      element.destroy
+    end
     @item.destroy
     respond_to do |format|
       format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
