5228c81fcac00e04460b6e3018b4c01c7a522fe2
selmenm <selmen.mhamdi@live.fr>
2019-03-19 11:58:13 +0100

    Adding Kaminari gem

--- app/controllers/variants_controller.rb
+++ app/controllers/variants_controller.rb
@@ -5,14 +5,14 @@ class VariantsController < ApplicationController
   # GET /variants.json
   def index
     if params[:category]
-      @variants = Variant.where(item_id: Item.where(category_id: Category.where(name: params[:category]).first.id).ids)
+      @variants = Variant.where(item_id: Item.where(category_id: Category.where(name: params[:category]).first.id).ids).order(:title)
       if @variants.empty?
         flash[:error] = "There are <b>#{@variants.count}</b> in this category".html_safe
       else
         flash[:notice] = "There are <b>${@variants.count}</b> in this category".html_safe
       end
     else
-      @variants = Variant.all
+      @variants = Variant.page(params[:page]).per(12)
     end
   end
 
