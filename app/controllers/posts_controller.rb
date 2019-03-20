a777a632f4740250f08301b3d83f1611a19c1145
ggoachet <goachetgerard@gmail.com>
2019-03-20 18:01:37 +0100

    Added pagination functions to Post pages - all files modified in accordance to kaminari's needs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.page(params[:page])
   end
 
   def postDashboard
