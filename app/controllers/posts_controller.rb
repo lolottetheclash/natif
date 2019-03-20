0b7c8b1a7c43207dcbda57f48b81a0db2815e01f
ggoachet <goachetgerard@gmail.com>
2019-03-20 11:06:42 +0100

    deleted now useless view since Post is now v2 - deleted now corresponding useless method in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,6 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
     @posts = Post.all
   end
 
-  def index_list
-    @posts = Post.all
-  end
-
   # GET /posts/1
   # GET /posts/1.json
   def show
