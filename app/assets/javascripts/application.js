ff8e62c7cc73ec072323088ce108d4d63859af80
guillaume-CHR <guillaume.christe@gmail.com>
2019-03-22 15:51:27 +0100

    Removal 4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,13 +24,17 @@
 //= require_tree .
 
 $(document).ready(function(){
-  setTimeout(function(){
-    $('#flash').remove();
-  }, 5000);
 
   App.rating()
 
-
+  let fullHeart = document.querySelectorAll(".oups"); 
+  function displayAlert() 
+  {
+    alert("Cet article est déjà présent dans votre wishlist!");
+  };
+  for (i = 0; i < fullHeart.length; i++) {
+    fullHeart[i].addEventListener("click", displayAlert);
+  }
 
   $('.post_list_layout').click(function (event) {
     $('#grid-layout').attr('hidden', true);
